d7be5a931f507bf02d0243ad80fbcf98777a4abb
andres <ydres6@gmail.com>
2020-11-11 12:35:07 -0300

    Add project files.

---
+++ Bicicletas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bicicletas.Startup))]
+namespace Bicicletas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
